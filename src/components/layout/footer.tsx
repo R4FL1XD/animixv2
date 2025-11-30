602236fabca5126e0bf58c987e36fdd4b8b7cdfc
Rafli Udin <rafliudin24@gmail.com>
2025-11-30 17:34:26 +0000

    terlalu kebanyakan seperti web stream biasanya saja (_for element p_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,8 +3,7 @@ export default function Footer() {
     <footer className="py-6 md:px-8 md:py-0 mt-8 border-t">
       <div className="container flex flex-col items-center justify-center gap-4 h-24">
         <p className="text-center text-sm leading-loose text-muted-foreground max-w-2xl">
-          Animix is a fan-made project for educational and portfolio purposes, not for commercial use.
-          All anime data is provided by an external API from Sankavollerei. This site is not affiliated with any official streaming service.
+          Disclaimer: This site does not store any files on its server. All contents are provided by non-affiliated third parties.
         </p>
       </div>
     </footer>
